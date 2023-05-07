1bbc90feedaeb57998db366688971e59c12bd482
Lê Công Tố <congto789@gmail.com>
2023-05-07 23:47:05 +0700

    Source code tiktok

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+// Layouts
+import { HeaderOnly } from '@/component/Layout';
+
+import Home from '@/pages/Home';
+import Following from '@/pages/Following';
+import Profile from '@/pages/Profile';
+import Update from '@/pages/Update';
+import Search from '@/pages/Search';
+
+// Public
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/update', component: Update, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+// Private
+const privateRoutes = [];
+
+export { privateRoutes, publicRoutes };
